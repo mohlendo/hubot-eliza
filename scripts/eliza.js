9d09b06de4889bc68e8dd8ff2a6875742245a029
Manuel Ohlendorf <moh@icnh.de>
2015-03-16 21:55:13 +0100

    eliza per room

--- scripts/eliza.js
+++ scripts/eliza.js
@@ -11,23 +11,45 @@
 
 var ElizaBot = require('../src/elizabot');
 
-var eliza;
-var justStarted;
+// in memory store for all eliza sessions
+var elizas = {};
+
 module.exports = function (robot) {
+
+    // Finds the room for most adaptors
+    function findRoom(msg) {
+        var room = msg.envelope.room;
+        if (room == undefined) {
+            room = msg.envelope.user.reply_to;
+        }
+        return room;
+    }
+
+    function findEliza(msg) {
+        var room = findRoom(msg);
+        return elizas[room];
+    }
+
+    function saveEliza(msg, eliza) {
+        var room = findRoom(msg);
+        elizas[room] = eliza;
+    }
 
     // start the eliza session
     robot.respond(/eliza$/i, function (msg) {
-        eliza = new ElizaBot();
+        var eliza = new ElizaBot();
+        eliza.justStarted = true;
+        saveEliza(msg, eliza);
         msg.reply(eliza.getInitial());
-        justStarted = true;
     });
 
     // end the eliza session
     robot.respond(/bye eliza$/i, function (msg) {
+        var eliza = findEliza(msg);
         if (eliza) {
             msg.reply(eliza.getFinal());
             eliza.reset();
-            eliza = undefined;
+            saveEliza(msg, undefined);
         } else {
             msg.reply("Sorry, but there is no running ELIZA session.");
         }
@@ -35,11 +57,20 @@ module.exports = function (robot) {
 
     // intercept EVERY message and parse it with eliza
     robot.respond(/(.+)$/i, function (msg) {
-        // we have to eliza - ignore
-        if (!eliza || justStarted) {
-            justStarted = false;
+        var eliza = findEliza(msg);
+
+        // we have no eliza - ignore
+        if (!eliza) {
             return;
         }
+
+        // ignore the first message
+        if (eliza.justStarted) {
+            eliza.justStarted = false;
+            return;
+        }
+
+        // now handle the message
         var txt = msg.match[1];
         msg.reply(eliza.transform(txt));
     });
