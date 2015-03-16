0a01d7694ff3d4f24a0a76e62fcb509511168664
Manuel Ohlendorf <moh@icnh.de>
2015-03-16 21:09:28 +0100

    cleanup

--- scripts/eliza.js
+++ scripts/eliza.js
@@ -1,9 +1,6 @@
 // Description:
 //   Hubot script for the famous Robotic Rogerian Therapist (ELIZA)
 //
-// Dependencies:
-//   "hubot-eliza": "latest"
-//
 // Commands:
 //   hubot eliza - Begin session with the Robotic Rogerian Therapist
 //   hubot bye eliza - End session
@@ -12,35 +9,26 @@
 // Author:
 //   Manuel Ohlendorf <m.ohlendorf@gmail.com>
 
-//var ElizaBot = require('../src/elizabot');
+var ElizaBot = require('../src/elizabot');
 
-//var eliza;
-
+var eliza;
 module.exports = function (robot) {
 
-    //function startSession(msg) {
-    //    eliza = new ElizaBot();
-    //    msg.reply(eliza.getInitial());
-    //}
-    //
-    //function endSession(msg) {
-    //    if (eliza) {
-    //        msg.reply(eliza.getFinal());
-    //    } else {
-    //        msg.reply("Sorry, but there is no running ELIZA session.");
-    //    }
-    //}
-
     robot.respond(/eliza$/i, function (msg) {
-        msg.send("ELIZA");
-        //startSession(msg);
+        eliza = new ElizaBot();
+        msg.reply(eliza.getInitial());
     });
 
     robot.respond(/bye eliza$/i, function (msg) {
-        msg.send("ELIZA");
-        //endSession(msg);
+        if (eliza) {
+            msg.reply(eliza.getFinal());
+        } else {
+            msg.reply("Sorry, but there is no running ELIZA session.");
+        }
     });
-    robot.respond(/eliza help$/i, function (msg) {
-       msg.send("ELIZA");
-    });
+
+    robot.respont(/eliza (w+)$/, function (msg) {
+        var txt = msg.match[1];
+        msg.reply(eliza.transform(txt));
+    })
 };
